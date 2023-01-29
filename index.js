69cc57ff3838b18d3ef5a583b5fa056237e9a954
caoimhejyoti <caoimhejyoti@gmail.com>
2023-01-29 16:01:45 +0800

    new file= different code direction. needs testing

--- index.js
+++ index.js
@@ -1,180 +1,154 @@
 // DESCRIPTION: node packages used within application.
 const inquirer = require('inquirer');
 const fs = require ("fs");
-var stringValidate = require ("string-validate");
-//FIXME: where did this come from?
-const { number } = require("yargs");
 
-// DESCRIPTION: Array of questions for user input
-const questions = [
-    {name: "teamName",
-    type: "input",
-    message: "What is the Team name?",
-    validate: function (userInput) {
-                if (typeof userInput === "string"){
-                    return true
-                }else{
-                    return ("Please enter a valid name");
-                };
-    }},
+const Employee = require('./Employee.js');
+const Manager = require('./Manager.js');
+const Engineer = require('./Engineer.js');
+const Intern = require('./Intern.js');
 
-    {name: "managerName",
-    type: "input",
-    message: "What is the Team Manager's name?",
-    // validate: function (userInput) {
-    //             if (typeof userInput === "string"){
-    //                 return true
-    //             }else{
-    //                 return ("Please enter a valid name");
-    //             };
-    default: 'My Team',
-    },
+const team = [];
 
-    {name: "managerID",
-    type: "number",
-    message: "What is the Team Manager's ID?",
-    validate: function (userInput) {
-                if (typeof userInput === number){
-                    return true
-                }else{
-                    return ("Please enter a valid ID number");
-                };
-    }},
-
-    {name: "managerEmail",
-    type: "input",
-    message: "What is the Team Manager's email address?",
-    validate: function (userInput) {
-                const symbols = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                if (symbols.test(String(userInput).toLowerCase())) {
-                    return true
-                } else {
-                    return ('Please enter a valid email!');
-                };
-            },
-    },
-
-    {name: "managerOffice",
-    type: "number",
-    message: "What is the Team Manager's office number?",
-    validate: function (userInput) {
-                if (typeof userInput === number){
-                    return true
-                }else{
-                    return ("Please enter a valid office number");
-                };
-    }},
-
-    {name: "addTeamMember",
-    type: "list",
-    message: "Which type of Team Member would you like to add?",
-    choices: [
-        'Engingeer',
-        'Intern',
-        "I don't want to add a new team member",
-    ],
-    default: 'Engingeer',
-    },
-
-    {name: "engineerName",
-    type: "input",
-    message: "What is the Engineer's name?",
-    validate: function (userInput) {
-                if (typeof userInput === "string"){
-                    return true
-                }else{
-                    return ("Please enter a valid name");
-                };
-    }},
-
-    {name: "engineerID",
-    type: "number",
-    message: "What is the Engineer's ID?",
-    validate: function (userInput) {
-                if (typeof userInput === number){
-                    return true
-                }else{
-                    return ("Please enter a valid ID number");
-                };
-    }},
-
-    {name: "engineerEmail",
-    type: "input",
-    message: "What is the Engineer's email address?",
-    validate: function (userInput) {
-                const symbols = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                if (symbols.test(String(userInput).toLowerCase())) {
-                    return true
-                } else {
-                    return ('Please enter a valid email!');
-                };
-            },
-    },
-    {name: "internName",
-    type: "input",
-    message: "What is the Intern's name?",
-    validate: function (userInput) {
-                if (typeof userInput === "string"){
-                    return true
-                }else{
-                    return ("Please enter a valid name");
-                };
-    }},
-
-    {name: "internID",
-    type: "number",
-    message: "What is the Intern's ID?",
-    validate: function (userInput) {
-                if (typeof userInput === number){
-                    return true
-                }else{
-                    return ("Please enter a valid ID number");
-                };
-    }},
-
-    {name: "internEmail",
-    type: "input",
-    message: "What is the Intern's email address?",
-    validate: function (userInput) {
-                const symbols = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                if (symbols.test(String(userInput).toLowerCase())) {
-                    return true
-                } else {
-                    return ('Please enter a valid email!');
-                };
-            },
-    },
-
-    {name: "internSchool",
-    type: "input",
-    message: "What is the Intern's school",
-    validate: function (userInput) {
-                if (typeof userInput === "string"){
-                    return true
-                }else{
-                    return ("Please enter a valid school name");
-                };
-    }},
-
-];
-
-//DESCRIPTION: links to generateHTML.js file for exported packages.
-const generateHTML = require(`./src/generateHTML`);
-
-// DESCRIPTION: function to write HTML file
-function writeToFileFnc() {
+function firstQuestion(){
     inquirer
-        .prompt (questions)
-            .then ((data) => {
-                fs.writeFile(`${name}.html`, generateHTML(data), (err) =>
-                err ? console.error(err) : console.log('Team webpage created!'))
-            });
-};
+        .prompt ([
+            {name: "managerName",
+            type: "input",
+            message: "What is the Team Manager's name?",
+            validate: function (userInput) {
+                        if (typeof userInput === "string"){
+                            return true
+                        }else{
+                            return ("Please enter a valid name");
+                        };
+            }},
 
-//DESCRIPTION: Function to initialize app
-function initFnc() {
-    writeToFileFnc();
+            {name: "managerID",
+            type: "number",
+            message: "What is the Team Manager's ID?",
+            validate: function (userInput) {
+                        if (typeof userInput === number){
+                            return true
+                        }else{
+                            return ("Please enter a valid ID number");
+                        };
+            }},
+
+            {name: "managerEmail",
+            type: "input",
+            message: "What is the Team Manager's email address?",
+            validate: function (userInput) {
+                        const symbols = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                        if (symbols.test(String(userInput).toLowerCase())) {
+                            return true
+                        } else {
+                            return ('Please enter a valid email!');
+                        };
+                    },
+            },
+
+            {name: "managerOffice",
+            type: "number",
+            message: "What is the Team Manager's office number?",
+            validate: function (userInput) {
+                        if (typeof userInput === number){
+                            return true
+                        }else{
+                            return ("Please enter a valid office number");
+                        };
+            }},
+        ])
+        .then ((data) => {
+            const manager = new Manager(
+                data.managerName,
+                data.managerID,
+                data.managerEmail,
+                data.managerOffice,
+            );
+            console.log(manager);
+            team.push(manager);
+            addTeamMember();
+            console.log(team);
+        });
 }
 
-//DESCRIPTION: Function call to initialize app
-initFnc();
+function addTeamMember(){
+    inquirer
+        .prompt([
+            {name: "role",
+            type: "list",
+            message: "Which type of Team Member would you like to add?",
+            choices: [
+                'Engingeer',
+                'Intern',
+                "I don't want to add a new team member",
+            ],
+            default: 'Engingeer',
+            },
+        ])
+        .then((data) => {
+            if (data.role === 'Engineer'){
+                engineerQuestions();
+            }else if (data.role === 'Intern'){
+                internQuestions();
+            } else {
+                renderHTMLFile();
+            };
+        });
+};
+
+const employeeQuestions = [
+    {name: `${role}Name`,
+    type: "input",
+    message: `What is the ${role}'s name?`,
+    validate: function (userInput) {
+                if (typeof userInput === "string"){
+                    return true
+                }else{
+                    return ("Please enter a valid name");
+                };
+    }},
+
+    {name: `${role}ID`,
+    type: "number",
+    message: `What is the ${role}'s ID?`,
+    validate: function (userInput) {
+                if (typeof userInput === number){
+                    return true
+                }else{
+                    return ("Please enter a valid ID number");
+                };
+    }},
+
+    {name: `${role}Email`,
+    type: "input",
+    message: `What is the ${role}'s email address?`,
+    validate: function (userInput) {
+                const symbols = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (symbols.test(String(userInput).toLowerCase())) {
+                    return true
+                } else {
+                    return ('Please enter a valid email!');
+                };
+            },
+    }
+]
+
+function engineerQuestions() {
+    inquirer
+        .prompt([
+            {employeeQuestions},
+            {name: "github",
+            type: "input",
+            message: "What is the Engineers's GitHub username?",
+            when: (answers) => answers.addTeamMember === 'Intern',
+            validate: function (value) {
+                        return axios.get(`https://api.github.com/users/${value}`)
+                            .then (data => {
+                                return true;
+                            })
+                            .catch (err => {
+                                return (`Please enter a valid GitHub username`);
+                            });
+            }},
+        ])
