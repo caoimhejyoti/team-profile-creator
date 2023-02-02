41f08a04b29e20f97b22b0e709e617adaa5f48fb
caoimhejyoti <caoimhejyoti@gmail.com>
2023-02-02 09:02:50 +0800

    engineer tests written and passing

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,4 +1,21 @@
-// const Employee = require("../lib/Employee.js")
-// const Engineer = require("../lib/Engineer.js")
+const Employee = require("../lib/Employee.js")
+const Engineer = require("../lib/Engineer.js")
 
+test("should create a new engineer class", () => {
+    const engineer = new Engineer("Sarah", 1, "test@test.com", "caoimhejyoti");
+    expect (engineer.name).toBe("Sarah");
+    expect (engineer.id).toBe(1);
+    expect (engineer.email).toBe("test@test.com");
+    expect (engineer.github).toBe("caoimhejyoti");
+});
+
+test("should get name via getGithub", () => {
+    const github = new Engineer("Sarah", 1, "test@test.com", "caoimhejyoti");
+    expect (github.getGithub()).toBe("caoimhejyoti");
+});
+
+test("should get name via getRole", () => {
+    const role = new Engineer("Sarah", 1, "test@test.com", "caoimhejyoti");
+    expect (role.getRole()).toBe("Engineer");
+});
 
